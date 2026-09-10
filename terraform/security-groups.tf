6b9569c213fcb0272c0f10bbf57190827958f355
hotfixdevops <hotfixdevops@gmail.com>
2026-09-10 15:42:32 +0000

    Add EC2 security group

---
+++ terraform/security-groups.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "ec2" {
+  name        = "${var.project}-ec2-sg"
+  description = "Security group for EC2 instances"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project}-ec2-sg"
+    Environment = var.environment
+    Project     = var.project
+  }
+}

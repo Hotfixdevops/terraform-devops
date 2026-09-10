1f6757aa84436e088a9e53bee55c8bfbde7caf9f
hotfixdevops <hotfixdevops@gmail.com>
2026-09-10 15:34:26 +0000

    Add VPC networking outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,28 @@ output "vpc_cidr" {
   description = "CIDR block of the VPC"
   value       = module.vpc.vpc_cidr
 }
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = module.vpc.private_subnet_ids
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = module.vpc.internet_gateway_id
+}
+
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = module.vpc.nat_gateway_id
+}
+
+output "nat_eip_public_ip" {
+  description = "Public IP address of the NAT Gateway Elastic IP"
+  value       = module.vpc.nat_eip_public_ip
+}
